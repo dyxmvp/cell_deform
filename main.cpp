2b3c848dd24b592e2e35974d9f22411e1b808153
DYX <dyxmvp@gmail.com>
2016-06-13 21:57:20 -0400

    change output after update to 0.35*cutI

--- main.cpp
+++ main.cpp
@@ -290,8 +290,8 @@ void Cyto::compute(int partNum, int ext)
 				centroids = Centroid(i);
 				//cout << "result: " << "i = " << i << ",   " << diameter; cout << ",   "; cout << deform1 << ",  " << deform2 << endl << endl;
 				//cout << ",   "; cout << i;  cout << '\n'; // Display the results
-				sizefile << diameter << endl;  // i << ",   " << diameter << endl;  // "\t" << deformations[i - 1] << "\t" << i + 8 << '\t' << i + 9 << endl;
-				deformfile << deformations << endl; // deform1 << ",   " << deform2 << endl;
+				sizefile << i << ",   " << diameter << endl;  // "\t" << deformations[i - 1] << "\t" << i + 8 << '\t' << i + 9 << endl;
+				deformfile << deform1 << ",   " << deform2 << endl;
 				centroidfile << centroids << endl;
 			}
 
